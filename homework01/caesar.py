bcfcf9db7e092f4ba8824bf5e4ddbb2bf6928e6a
Андрей Денисович  Якунин <113021177+Andrucrut@users.noreply.github.com>
2022-10-12 15:49:24 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,7 +1,6 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
-
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -11,19 +10,19 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
-
-    new_word = []
-    ciphertext = ""
-    for i in plaintext:
-        if ord(i) >= 65 and ord(i) <= 90:
-            ciphertext= chr((((ord(i) - 65) + shift) % 26) + 65)
+    new_word = []  # новое слово
+    ciphertext = ""  # новая буква
+    for i in plaintext:  # i буква
+        if ord(i) >= 65 and ord(i) <= 90:  # от A до Z
+            ciphertext = chr((((ord(i) - 65) + shift) % 26) + 65)  # %26 чтобы у нас послдний символ был тем что надо
             new_word.append(ciphertext)
-        elif ord(i) >= 97 and ord(i) <= 122:
+        elif ord(i) >= 97 and ord(i) <= 122:  # проверка маленьких букв
             ciphertext = chr((((ord(i) - 97) + shift) % 26) + 97)
             new_word.append(ciphertext)
         else:
             new_word.append(i)
-    return "".join(new_word)
+    return "".join(new_word)  # возвращение в качестве строки
+
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     new_word = []
@@ -38,5 +37,3 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
         else:
             new_word.append(i)
     return "".join(new_word)
-
-

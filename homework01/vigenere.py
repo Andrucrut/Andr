703a37a0cc2f44ff01d607119d5bc67f45d207c4
Андрей Денисович  Якунин <113021177+Andrucrut@users.noreply.github.com>
2022-10-12 16:14:18 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,13 +9,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
 
-    keyword *= len(plaintext) // len(keyword) + 1  # Мы подгоняем кол-во символов в ключе к кол-во символом в тексте
+    keyword *= (
+        len(plaintext) // len(keyword) + 1
+    )  # Мы подгоняем кол-во символов в ключе к кол-во символом в тексте
     ciphertext = ""
     for i, j in enumerate(plaintext):  # i = index  j=bukva
-        if (keyword[i] == "a" or keyword[i] == "A"):  # если у нас в ключе будет буква A, то закадируется также A
+        if (
+            keyword[i] == "a" or keyword[i] == "A"
+        ):  # если у нас в ключе будет буква A, то закадируется также A
             ciphertext += j
         else:
-            ciphertext += chr((ord(j) + ord(keyword[i])) % 26 + 65)  # сумму остаток от деления на 26 и прибовляем 65(для того чтобы не выходило из таблицы)
+            ciphertext += chr(
+                (ord(j) + ord(keyword[i])) % 26 + 65
+            )  # сумму остаток от деления на 26 и прибовляем 65(для того чтобы не выходило из таблицы)
     return ciphertext
 
 
@@ -33,7 +39,7 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     keyword *= len(ciphertext) // len(keyword) + 1
     plaintext = ""
     for i, j in enumerate(ciphertext):
-        if (keyword[i] == "a" or keyword[i] == "A"):
+        if keyword[i] == "a" or keyword[i] == "A":
             plaintext += j
         else:
             plaintext += chr((ord(j) - ord(keyword[i])) % 26 + 65)

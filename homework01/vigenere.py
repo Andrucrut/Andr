014f661485894a64af67be6f1d85825b1cdac058
Андрей Денисович  Якунин <113021177+Andrucrut@users.noreply.github.com>
2022-10-12 15:50:50 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,15 +9,16 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
 
-    keyword *= len(plaintext) // len(keyword) + 1
+    keyword *= len(plaintext) // len(keyword) + 1  # Мы подгоняем кол-во символов в ключе к кол-во символом в тексте
     ciphertext = ""
-    for i, j in enumerate(plaintext):
-        if (keyword[i] == 'a' or keyword[i] == "A"):
+    for i, j in enumerate(plaintext):  # i = index  j=bukva
+        if (keyword[i] == 'a' or keyword[i] == "A"):  # если у нас в ключе будет буква A, то закадируется также A
             ciphertext += j
         else:
-            bukva = (ord(j) + ord(keyword[i]))
-            ciphertext += chr(bukva % 26 + 65)
+            ciphertext += chr((ord(j) + ord(keyword[
+                                                i])) % 26 + 65)  # сумму остаток от деления на 26 и прибовляем 65(для того чтобы не выходило из таблицы)
     return ciphertext
+
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
@@ -36,6 +37,5 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
         if (keyword[i] == 'a' or keyword[i] == "A"):
             plaintext += j
         else:
-            bukva = (ord(j) - ord(keyword[i]))
-            plaintext += chr(bukva % 26 + 65)
+            plaintext += chr((ord(j) - ord(keyword[i])) % 26 + 65)
     return plaintext

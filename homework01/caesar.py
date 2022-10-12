3da49f463b4d35774eb9ed8882dab980f3d3aad9
Andrucrut <andrucrut@mail.ru>
2022-10-12 16:28:58 +0300

    done

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -39,3 +40,47 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
         else:
             new_word.append(i)
     return "".join(new_word)
+=======
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+
+    new_word = []
+    ciphertext = ''
+    for i in plaintext:
+        if ord(i) >= 65 and ord(i) <= 90:
+            ciphertext= chr((((ord(i) - 65) + shift) % 26) + 65)
+            new_word.append(ciphertext)
+        elif ord(i) >= 97 and ord(i) <= 122:
+            ciphertext = chr((((ord(i) - 97) + shift) % 26) + 97)
+            new_word.append(ciphertext)
+        else:
+            new_word.append(i)
+    return "".join(new_word)
+
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    new_word = []
+    new_letter = ''
+    for i in ciphertext:
+        if ord(i) >= 65 and ord(i) <= 90:
+            new_letter = chr((((ord(i) - 65) - shift) % 26) + 65)
+            new_word.append(new_letter)
+        elif ord(i) >= 97 and ord(i) <= 122:
+            new_letter = chr((((ord(i) - 97) - shift) % 26) + 97)
+            new_word.append(new_letter)
+        else:
+            new_word.append(i)
+    return "".join(new_word)
+
+
+>>>>>>> Stashed changes
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -44,3 +45,46 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
         else:
             plaintext += chr((ord(j) - ord(keyword[i])) % 26 + 65)
     return plaintext
+=======
+def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+    """
+    Encrypts plaintext using a Vigenere cipher.
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+
+    keyword *= len(plaintext) // len(keyword) + 1
+    ciphertext = ""
+    for i, j in enumerate(plaintext):
+        if (keyword[i] == 'a' or keyword[i] == "A"):
+            ciphertext += j
+        else:
+            bukva = (ord(j) + ord(keyword[i]))
+            ciphertext += chr(bukva % 26 + 65)
+    return ciphertext
+
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+
+    keyword *= len(ciphertext) // len(keyword) + 1
+    plaintext = ""
+    for i, j in enumerate(ciphertext):
+        if (keyword[i] == 'a' or keyword[i] == "A"):
+            plaintext += j
+        else:
+            bukva = (ord(j) - ord(keyword[i]))
+            plaintext += chr(bukva % 26 + 65)
+    return plaintext
+>>>>>>> Stashed changes

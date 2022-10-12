0e85a123189cbafeed1fc98c57327b541e9c3e26
Андрей Денисович  Якунин <113021177+Andrucrut@users.noreply.github.com>
2022-10-12 16:13:45 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -48,7 +48,9 @@ def multiplicative_inverse(e: int, phi: int) -> int:
         d += 1
 
 
-def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+def generate_keypair(
+    p: int, q: int
+) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
@@ -93,7 +95,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
+    plain = [chr((char**key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 

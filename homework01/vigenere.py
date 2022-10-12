c07ad408c77a118fcb8011c7010f827de7ed113d
Андрей Денисович  Якунин <113021177+Andrucrut@users.noreply.github.com>
2022-10-12 18:26:55 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -14,9 +14,9 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     )  # Мы подгоняем кол-во символов в ключе к кол-во символом в тексте
     ciphertext = ""
     for i, j in enumerate(plaintext):  # i = index  j=bukva
-        if (
+        if 
             keyword[i] == "a" or keyword[i] == "A"
-        ):  # если у нас в ключе будет буква A, то закадируется также A
+        :  # если у нас в ключе будет буква A, то закадируется также A
             ciphertext += j
         else:
             ciphertext += chr(
@@ -43,46 +43,4 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
             plaintext += j
         else:
             plaintext += chr((ord(j) - ord(keyword[i])) % 26 + 65)
-    return plaintext
-
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-
-    keyword *= len(plaintext) // len(keyword) + 1
-    ciphertext = ""
-    for i, j in enumerate(plaintext):
-        if (keyword[i] == 'a' or keyword[i] == "A"):
-            ciphertext += j
-        else:
-            bukva = (ord(j) + ord(keyword[i]))
-            ciphertext += chr(bukva % 26 + 65)
-    return ciphertext
-
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-
-    keyword *= len(ciphertext) // len(keyword) + 1
-    plaintext = ""
-    for i, j in enumerate(ciphertext):
-        if (keyword[i] == 'a' or keyword[i] == "A"):
-            plaintext += j
-        else:
-            bukva = (ord(j) - ord(keyword[i]))
-            plaintext += chr(bukva % 26 + 65)
     return plaintext
